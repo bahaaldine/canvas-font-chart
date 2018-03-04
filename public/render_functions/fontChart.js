aa35aaa4d0192ffffc98ef3737183398a07116cc
Bahaaldine Azarmi <bahaaldine@gmail.com>
2018-03-03 21:56:45 -0800

    adding odometer

--- public/render_functions/fontChart.js
+++ public/render_functions/fontChart.js
@@ -3,6 +3,8 @@ import React from 'react';
 import * as FontAwesome from 'react-icons/lib/fa';
 import './fontChart.css';
 import { uniqueId } from 'lodash';
+import Odometer from './odometer';
+import './odometer-theme-default.css';
 
 export const fontChart = () => ({
   name: 'fontChart',
@@ -13,7 +15,8 @@ export const fontChart = () => ({
 
     const color = config.color;
     const uuid = uniqueId();
-    const domId = 'fontChart'+uuid;
+
+    const domId = $('div', domNode).attr('id') || 'fontChart'+uuid;
 
     const createIcon = (name) => {
       let Icon = FontAwesome['Fa'+name];
@@ -25,36 +28,51 @@ export const fontChart = () => ({
 
     const font = (
       <div id={domId} name={config.name} className='fontChart'>
-        <div>
+        <div className="font">
           { createIcon(config.name) }
         </div>
+        <div id='odometer' className="odometer"></div>
       </div>
     )
-    if ( $('svg', domNode).length != 0 && ($('div', domNode).attr('name') != config.name) ) {
+    if ( $('svg', domNode).length != 0 && $('div', domNode).attr('name') != config.name )  {
       ReactDOM.unmountComponentAtNode(domNode);
-    } 
+    }
+    
     ReactDOM.render(font, domNode, () => {
 
-      const size = Math.min( 
-        Math.round(domNode.clientHeight*0.7), 
-        Math.round(domNode.clientWidth*0.7) 
+      let size = Math.min( 
+        Math.round(domNode.clientHeight*0.6), 
+        Math.round(domNode.clientWidth*0.6) 
       );
       const percentage = (100-config.value)/100;
       const prevPercentage = $('#'+domId).attr('percentage') || 0;
 
       // keep the percentage there for state.
       $('#'+domId, domNode).attr('percentage', percentage);
-      
-      $('#'+domId, domNode).css('font-size', size+'px'); 
-      $('#'+domId+' svg g', domNode).attr('fill', 'url(#grad'+uuid+')');
+
+      $('#'+domId + ' .font', domNode).css('font-size', size+'px'); 
+      $('#'+domId+' svg g', domNode).attr('fill', 'url(#grad' + uuid + ')');
       $('#'+domId+' svg defs', domNode).remove();
-      $('#'+domId+' svg g', domNode).before('<defs><linearGradient id="grad'+uuid+'" x1="0" x2="0" y1="0" y2="100%"><stop offset="'+percentage+'" stop-color="white"><animate begin="0s"  attributeName="offset" from="'+prevPercentage+'" to="'+percentage+'" dur="500ms" repeatCount="1" keySplines="0.1 0.8 0.2 1;0.1 0.8 0.2 1;0.1 0.8 0.2 1;0.1 0.8 0.2 1;0.1 0.8 0.2 1;0.1 0.8 0.2 1" keyTimes="0;0.22;0.33;0.55;0.66;0.88;1" calcMode="spline" /></stop><stop offset="0%" stop-color="'+color+'"></stop></linearGradient></defs>');
+      $('#'+domId+' svg g', domNode).before('<defs><linearGradient id="grad'+uuid+'" x1="0" x2="0" y1="0" y2="100%"><stop offset="'+percentage+'" stop-color="white"><animate begin="0s"  attributeName="offset" from="'+prevPercentage+'" to="'+percentage+'" dur="800ms" repeatCount="1" keySplines="0.1 0.8 0.2 1;0.1 0.8 0.2 1;0.1 0.8 0.2 1;0.1 0.8 0.2 1;0.1 0.8 0.2 1;0.1 0.8 0.2 1" keyTimes="0;0.22;0.33;0.55;0.66;0.88;1" calcMode="spline" /></stop><stop offset="0%" stop-color="'+color+'"></stop></linearGradient></defs>');
+      
+      //$('#'+domId+' div.odometer', domNode)[0].innerHTML = Math.round(config.value);
+      $('#'+domId + ' .odometer', domNode).css('font-size', size*0.3+'px'); 
+      $('#'+domId + ' .odometer', domNode).css('color', color); 
+
+      const meter = 100 - $('#'+domId, domNode).attr('percentage') * 100 || 0;
+      const odometer = new Odometer({
+        el: document.querySelector('#'+domId+' div.odometer', domNode),
+        value: meter,
+      });
+
+      odometer.update(config.value);
 
       // rendering SVG changes
-      $('#'+domId).html($('#'+domId).html());
+      $('#'+domId+' .font').html($('#'+domId+' .font').html());
 
       handlers.done();
     });
+
 
 
     handlers.onDestroy(() => {
@@ -62,7 +80,12 @@ export const fontChart = () => ({
     });
 
     handlers.onResize( () => {
-      $('.fontChart', domNode).css('font-size', Math.round(domNode.clientHeight*0.7)+'px'); 
+      let size = Math.min( 
+        Math.round(domNode.clientHeight*0.6), 
+        Math.round(domNode.clientWidth*0.6) 
+      );
+      $('#'+domId +' .font', domNode).css('font-size', size+'px'); 
+      $('#'+domId +' .odometer', domNode).css('font-size', size*0.3+'px'); 
     });
 
     return handlers.done();
